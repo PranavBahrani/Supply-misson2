c78ad04df753719d2e507fb2d2060abbbdf4bdbb
PranavBahrani <69100517+PranavBahrani@users.noreply.github.com>
2020-09-10 09:14:56 +0530

    Add files via upload

---
+++ Package.js
@@ -0,0 +1,13 @@
+class Package{
+    constructor(x,y,width,height){
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x,y,width,height);
+        World.add(world,this.body);
+        
+    }
+    display(){
+        rectMode(CENTER) 
+        rect(this.body.position.x,this.body.position.y,this.width,this.height); 
+    }
+}
